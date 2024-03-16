65fd5e48bb6063c3617d635f23aeef3d9f099d92
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-03-16 15:47:45 -0700

    Delete HW9/pySol directory

--- HW9/pySol/TreeNode.py
+++
@@ -1,7 +0,0 @@
- class TreeNode(object):
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
-
-     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
